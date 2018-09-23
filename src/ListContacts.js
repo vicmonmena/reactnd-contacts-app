4679dfc6eaccb723d2eece7d4009db0a43b41876
vmontanomena <vicente.montanomena@telefonica.com>
2018-09-23 21:05:59 +0200

    Transformando ListCOntacts en componente funcional

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,32 +1,28 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListContacts extends Component {
-  render() {
-    console.log('ListContacts props: ', this.props.contacts)
-    return(
-      <ol className="list-contacts">
-        {this.props.contacts.map(contact => (
-          <li key={contact.id} className="contact-list-item">
-            <div 
-              className="contact-avatar" 
-              style={{
-                backgroundImage: `url(${contact.avatarURL})`
-              }}
-            >
-            </div>
-            <div 
-              className="contact-details" >
-              <p>{contact.name}</p>
-              <p>@{contact.handle}</p>
-            </div>
-            <button className="contact-remove">
-              Remove
-            </button>
-          </li>
-        ))}
-      </ol>
-    )
-  }
-}
+const ListContacts = (props) => (
+
+  <ol className="list-contacts">
+    {props.contacts.map(contact => (
+      <li key={contact.id} className="contact-list-item">
+        <div 
+          className="contact-avatar" 
+          style={{
+            backgroundImage: `url(${contact.avatarURL})`
+          }}
+        >
+        </div>
+        <div 
+          className="contact-details" >
+          <p>{contact.name}</p>
+          <p>@{contact.handle}</p>
+        </div>
+        <button className="contact-remove">
+          Remove
+        </button>
+      </li>
+    ))}
+  </ol>
+)
 
 export default ListContacts;
