7a7829f50935f4988ae44e7e1b8b11efd82a4f8c
vmontanomena <vicente.montanomena@telefonica.com>
2018-10-03 22:05:27 +0200

    Handle submit

--- src/CreateContact.js
+++ src/CreateContact.js
@@ -1,9 +1,38 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import ImageInput from './ImageInput';
+import serializeForm from 'form-serialize';
 
 class CreateContact extends Component {
+  
+  handleSubmit = event => {
+    event.preventDefault();
+    const values = serializeForm(event.target, {hash: true});
+    // console.log('values: ' + values);
+    if (this.props.onCreateContact)
+    this.props.onCreateContact(values);
+  }
+
   render() {
     return(
-      <div>Create contact form</div>
+      <div>
+        <Link
+          to='/'
+          className="close-create-contact"
+        />
+        <form className="create-contact-form" onSubmit={this.handleSubmit}>
+          <ImageInput
+            className="create-contact-avatar-input"
+            name="avatarURL"
+            maxHeight={64}
+          />
+          <div className="create-contact-details">
+            <input type="text" name="name" placeholder="Name"/>
+            <input type="text" name="handle" placeholder="Handle"/>
+          </div>
+          <button>Add contact</button>
+        </form>
+      </div>
     );
   }
 }
